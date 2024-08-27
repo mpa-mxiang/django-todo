ce19dbad29c6466ff3ec2406686f03f335a6ff5e
Jiachun Xiang <jcxiang1230@gmail.com>
2024-08-27 00:04:30 -0400

    create admin file to allow new signup user to store

---
+++ todo/todo/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from todo.models import Todo
+admin.site.register(Todo)
--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from todo import models
+from todo.models import Todo
 
 def signup(request):
+    if request.method=="POST":
+        frm = request.POST.get('frm')
+        email_id = request.POST.get('emailId')
+        pwd = request.POST.get('pwd')
+        new_user = User.object.create_user(frm, email_id, pwd)
+        new_user.save()
+
     return render(request, 'signup.html')
