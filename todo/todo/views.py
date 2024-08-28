fe958fcdbf62427ca11615d5a7983b34d78a5c18
Jiachun Xiang <jcxiang1230@gmail.com>
2024-08-27 23:19:44 -0400

    add error message for creating a duplicate user name

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -3,16 +3,24 @@ from django.contrib.auth.models import User
 from todo import models
 from todo.models import Todo
 
+from django.db import IntegrityError
+
 def signup(request):
-    if request.method=="POST":
+    error_message = None
+    if request.method == "POST":
         frm = request.POST.get('frm')
         email_id = request.POST.get('emailId')
         pwd = request.POST.get('pwd')
-        new_user = User.objects.create_user(frm, email_id, pwd)
-        new_user.save()
-        return redirect('/login')
+        
+        try:
+            new_user = User.objects.create_user(frm, email_id, pwd)
+            new_user.save()
+            return redirect('/login')
+        except IntegrityError:
+            error_message = "Username already exists. Please choose a different username."
+    
+    return render(request, 'signup.html', {'error_message': error_message})
 
-    return render(request, 'signup.html')
 
 def login(request):
 
