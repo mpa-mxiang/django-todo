2a448a99b43d032b3e1fc2636604d8a45e105170
Jiachun Xiang <jcxiang1230@gmail.com>
2024-08-31 00:11:22 -0400

    fix edit_todo url and views

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.signup),
     path('login/', views.user_logn),
     path('todo/', views.todo),
-    path('edit_todo/<init:srno>', views.edit_todo, name='edit_todo')
+    path('edit_todo/<int:srno>', views.edit_todo, name='edit_todo')
 ]
--- todo/todo/views.py
+++ todo/todo/views.py
@@ -6,27 +6,28 @@ from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required
 
+
 def signup(request):
 
-    error_message = None    
+    error_message = None
     if request.method == "POST":
         frm = request.POST.get('frm')
         email_id = request.POST.get('emailId')
         pwd = request.POST.get('pwd')
-        
+
         try:
             new_user = User.objects.create_user(frm, email_id, pwd)
             new_user.save()
             return redirect('/login')
         except IntegrityError:
             error_message = "Username already exists. Please choose a different username."
-    
+
     return render(request, 'signup.html', {'error_message': error_message})
 
 
 def user_logn(request):
 
-    error_message = None    
+    error_message = None
     if request.method == "POST":
         frm = request.POST.get('frm')
         pwd = request.POST.get('pwd')
@@ -36,22 +37,41 @@ def user_logn(request):
             return redirect('/todo')
         else:
             error_message = "Wrong Username and/or Password, please try it again."
-    
+
     return render(request, 'login.html', {'error_message': error_message})
+
 
 @login_required(login_url='/login')
 def todo(request):
     if request.method == 'POST':
-        title=request.POST.get('title')
-        obj=models.Todo(title=title,user=request.user)
+        title = request.POST.get('title')
+        obj = models.Todo(title=title, user=request.user)
         obj.save()
-        user=request.user        
-        res=models.Todo.objects.filter(user=user).order_by('-date')
-        return redirect('/todo',{'res':res})
-        
-    
-    res=models.Todo.objects.filter(user=request.user).order_by('-date')
-    return render(request, 'todo.html',{'res':res,})
+        user = request.user
+        res = models.Todo.objects.filter(user=user).order_by('-date')
+        return redirect('/todo', {'res': res})
 
-def edit_todo(request):
-    pass
+    res = models.Todo.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'todo.html', {'res': res, })
+
+
+def edit_todo(request, srno):
+    # Fetch the todo item to be edited
+    obj = models.Todo.objects.get(srno=srno)
+
+    if request.method == 'POST':
+        # Update the title of the todo item
+        title = request.POST.get('title')
+        obj.title = title
+        obj.save()
+
+        # Fetch updated list of tasks for the logged-in user
+        user = request.user
+        res = models.Todo.objects.filter(user=user).order_by('-date')
+
+        # Redirect back to the todo page
+        return redirect('/todo')
+
+    # If it's a GET request, render the todo page
+    res = models.Todo.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'todo.html',{'res':res,})
